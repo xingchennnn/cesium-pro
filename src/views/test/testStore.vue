259039ce5937d29d1c8046c43617ed24ad0f48d9
饶奎 <2955095425@qq.com>
2025-01-07 16:13:02 +0800

    添加eventbus使用示例

---
+++ src/views/test/testStore.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import EventBus  from "@/utils/eventBus/EventBus";
+import { onMounted, onUnmounted } from "vue";
+
+onMounted(() => { 
+  // 监听test事件
+  EventBus.on("test", (data: any) => {
+    console.log(data);
+  })
+
+  setTimeout(() => { 
+    // 触发test事件
+    EventBus.emit("test", { name: "cesium" });
+  }, 2000)
+
+  onUnmounted(() => {
+    // 移除test事件监听
+    EventBus.off("test");
+  })
+})
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
